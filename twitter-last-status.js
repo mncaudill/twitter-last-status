45c7ff8e5b202f7294b4fdae9b35a8e948c240d9
Nolan Caudill <nolan@nolancaudill.com>
2009-07-23 18:59:18 -0400

    Better JavaScript, less global namespace pollution.
    Inspiration gotten from John Resig's retweet JS (http://ejohn.org/files/retweet.js)

--- twitter-last-status.js
+++ twitter-last-status.js
@@ -3,38 +3,44 @@
 * Author: Matthew Nolan Caudill (nolan@nolancaudill.com)
 */
 
-var twitter_username = 'nolancaudill';
-function twitter_callback (resp){
-    if(typeof resp.status == 'undefined') return;
+(function () {
 
-    var text = ' ' + resp.status.text;
-    text = text.replace(/(\s)(http(s)?:\/\/\S+)/gi, '$1<a href="$2">$2</a>');
-    text = text.replace(/(\s)@([A-Za-z0-9_]+)/g, '$1<a href="http://twitter.com/$2">@$2</a>').replace(/^\s/, '');
+    window.TwitterLastStatus = {
+        username: 'nolancaudill',
+        callback: function (resp) {
+            if (typeof resp.status === undefined) {
+                return;
+            }
 
-    var created_at = resp.status.created_at.split(' ');
-    created_at = new Date(created_at[1] + ' ' + created_at[2] + ' ' + created_at[5] + ' ' + created_at[3] + ' ' + created_at[4]);
+            var text, created_at, datetime, el;
 
-    var datetime = created_at.toLocaleTimeString() + ' on ' + (created_at.getMonth()+1) + '-' + created_at.getDate() + '-' + created_at.getFullYear();
+            text = ' ' + resp.status.text;
+            text = text.replace(/(\s)(http(s)?:\/\/\S+)/gi, '$1<a href="$2">$2</a>');
+            text = text.replace(/(\s)@([A-Za-z0-9_]+)/g, '$1<a href="http://twitter.com/$2">@$2</a>').replace(/^\s/, '');
 
-    var el = document.getElementById('twitter-last-status');
-    if(el) {
-        el.innerHTML = text + '<br/><a href="http://twitter.com/' + twitter_username + '">' + datetime + '</a>';
-    }
-}
-(function(){
+            created_at = resp.status.created_at.split(' ');
+            created_at = new Date(created_at[1] + ' ' + created_at[2] + ' ' + created_at[5] + ' ' + created_at[3] + ' ' + created_at[4]);
+
+            datetime = created_at.toLocaleTimeString() + ' on ' + (created_at.getMonth() + 1) + '-' + created_at.getDate() + '-' + created_at.getFullYear();
+
+            el = document.getElementById('twitter-last-status');
+            if (el) {
+                el.innerHTML = text + '<br/><a href="http://twitter.com/' + this.username + '">' + datetime + '</a>';
+            }
+        }
+    };
+
     function init() {
-        if(!twitter_username) return;
         var script = document.createElement('script');
-        script.src = 'http://twitter.com/users/show/' + twitter_username + '.json?callback=twitter_callback&ts=' + (new Date().getTime());
+        script.charSet = 'utf-8';
+        script.src = 'http://twitter.com/users/show/' + TwitterLastStatus.username + '.json?callback=TwitterLastStatus.callback&ts=' + (new Date().getTime());
         document.getElementsByTagName('head')[0].appendChild(script);
     }
-    var curr_onload = window.onload;
-    if(typeof curr_onload == 'function') {
-        window.onload = function(){
-            curr_onload();
-            init();
-        }
+
+    if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', init, false);
     } else {
-        window.onload = init;
+        window.attachEvent('onload', init);
     }
+
 })();
